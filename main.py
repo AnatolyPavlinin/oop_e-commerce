db4111bd2ae97b1c4c01fb677a28d955d2faccb8
Anatoly Pavlinin <anatolianych@gmail.com>
2026-03-08 14:54:39 +0300

    fixes

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 
     new_product = Product.new_product(
         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5}, [])
+         "quantity": 5})
     print(new_product.name)
     print(new_product.description)
     print(new_product.price)
